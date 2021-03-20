1aef475e039443db9440a10426971f2c9b950052
Brian Longmore <brianl.ext@gmail.com>
2021-03-20 03:33:07 -1000

    copied and pasted working example from discordgo github of pingpong bot

--- main.go
+++ main.go
@@ -1,41 +1,117 @@
+// package main
+
+// import (
+// 	"flag"
+// 	"log"
+
+// 	dg "github.com/bwmarrin/discordgo"
+// )
+
+// var (
+// 	token   string
+// 	session *dg.Session
+// 	err     error
+// )
+
+// func init() {
+// 	flag.StringVar(&token, "t", "", "Bot access token.")
+// 	flag.Parse()
+// }
+
+// func main() {
+// 	session, err = dg.New("Bot " + token)
+// 	if err != nil {
+// 		log.Fatal("failed to start bot:", err)
+// 	}
+
+// 	session.Open()
+// 	defer session.Close()
+
+// 	session.AddHandler(func(s *dg.Session, e *dg.MessageCreate) {
+// 		var (
+// 			msg string = e.Message.Content
+// 		)
+
+// 		_, err = s.ChannelMessageSend("imgcli", msg)
+// 		if err != nil {
+// 			log.Fatal("failed to send message:", err)
+// 		}
+
+// 	})
+// }
+
+// RESEARCH THE STUFF YOU DO NOT UNDERSTAND (lines 81, 92-94)
+
 package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	dg "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
+// Variables used for command line parameters
 var (
-	token   string
-	session *dg.Session
-	err     error
+	Token string
 )
 
 func init() {
-	flag.StringVar(&token, "t", "", "Bot access token.")
+	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
 func main() {
-	session, err = dg.New("Bot " + token)
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		log.Fatal("failed to start bot:", err)
+		fmt.Println("error creating Discord session,", err)
+		return
 	}
 
-	session.Open()
-	defer session.Close()
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
 
-	session.AddHandler(func(s *dg.Session, e *dg.MessageCreate) {
-		var (
-			msg string = e.Message.Content
-		)
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
-		_, err = s.ChannelMessageSend("imgcli", msg)
-		if err != nil {
-			log.Fatal("failed to send message:", err)
-		}
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
 
-	})
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
